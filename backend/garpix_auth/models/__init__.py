dbbc81995ada399dc110b2449f903c80f4fa1712
Alshevsky <99246723+Alshevsky@users.noreply.github.com>
2022-05-06 11:29:55 +0300

    Update __init__.py

--- backend/garpix_auth/models/__init__.py
+++ backend/garpix_auth/models/__init__.py
@@ -4,4 +4,5 @@ from .access_token import AccessToken  # noqa
 from .restore_password import RestorePasswordMixin # noqa
 from .users_config import GarpixUserConfig # noqa
 from .user_session import UserSession # noqa
-from .confirm import UserEmailPreConfirmMixin, UserEmailConfirmMixin, UserPhoneConfirmMixin, UserPhonePreConfirmMixin # noqa
+from .confirm import UserEmailPreConfirmMixin, UserEmailConfirmMixin, UserPhoneConfirmMixin, UserPhonePreConfirmMixin,\
+    UserEmailLinkConfirmMixin, UserEmailLinkPreConfirmMixin# noqa
