1175b92deac28d9aca9a752b330aab19870d6663
grivial <griviala@gmail.com>
2022-07-07 15:52:58 +0300

    Email confirmation by link updated

--- backend/garpix_user/mixins/models/confirm/__init__.py
+++ backend/garpix_user/mixins/models/confirm/__init__.py
@@ -1,3 +1,2 @@
 from .phone_confirm import UserPhoneConfirmMixin  # noqa
 from .email_confirm import UserEmailConfirmMixin  # noqa
-from .email_link_confirm import UserEmailLinkConfirmMixin, UserEmailLinkPreConfirmMixin # noqa
