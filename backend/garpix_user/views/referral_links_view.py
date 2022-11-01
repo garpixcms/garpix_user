ea4bcc26dffff023f3b2eee8a87a836cb00d2aeb
griviala <78097561+griviala@users.noreply.github.com>
2022-11-01 14:33:58 +0300

    3.0.0-rc6

--- backend/garpix_user/views/referral_links_view.py
+++ backend/garpix_user/views/referral_links_view.py
@@ -9,10 +9,11 @@ class ReferralLinkView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         hash = self.kwargs.get('hash', None)
         user = UserSession.get_or_create_user_session(self.request)
+        status = 'success'
         try:
             referral_instance = ReferralType.objects.get(referral_hash=hash)
             ReferralUserLink.objects.create(user=user, referral_type=referral_instance)
         except Exception:
-            pass
+            status = 'error'
 
-        return settings.GARPIX_USER.get('REFERRAL_REDIRECT_URL', '/')
+        return f"settings.GARPIX_USER.get('REFERRAL_REDIRECT_URL', '/')?status={status}"
