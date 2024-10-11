d274dbea054bc25b93af9ed125eed6515f017ce1
aftilion <afti_a@garpix.com>
2024-10-11 15:06:05 +0300

    EQATORSAAS-1039 Fix change number error

--- backend/garpix_user/serializers/phone_confirmation_serializer.py
+++ backend/garpix_user/serializers/phone_confirmation_serializer.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
-from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.serializerfields import PhoneNumberField as BasePhoneNumberField
 
+
+class PhoneNumberField(BasePhoneNumberField):
+    def run_validation(self, data=None):
+        try:
+            return super().run_validation(data)
+        except ValidationError:
+            raise ValidationError(_("Enter a valid phone number"))
 
 class PhoneConfirmSendSerializer(serializers.Serializer):
     phone = PhoneNumberField(required=False)
