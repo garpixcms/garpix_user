a0d8c1a0331112d74b06a237ea02a0aba5a02159
AnatolyBur <worm102@yandex.ru>
2024-11-18 18:06:13 +0300

    Версия - 3.10.0-rc26

--- backend/garpix_user/setup.py
+++ backend/garpix_user/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='garpix_user',
-    version='3.10.0-rc25',
+    version='3.10.0-rc26',
     description='',
     long_description=long_description,
     long_description_content_type='text/markdown',
