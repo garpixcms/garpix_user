73795e7e95b6887ca9cd83e0dcf48603e3ed71df
Igor Sorokin <sorokin_i@garpix.com>
2024-12-02 13:11:00 +0300

    Referral links tests

--- backend/garpix_user/tests/views/test_referral_links_view.py
+++ backend/garpix_user/tests/views/test_referral_links_view.py
@@ -7,16 +7,18 @@ from garpix_user.models import ReferralType
 BASE_GARPIX_USER_SETTINGS = getattr(settings, 'GARPIX_USER', dict())
 
 
-class LoginViewTest(TestCase):
-    def test_referral_links(self):
-        """
-        Check referral links functionality
-        """
-        if BASE_GARPIX_USER_SETTINGS.get('USE_REFERRAL_LINKS', False):
-            referral_link = ReferralType.objects.create(title='test_link')
-            response = self.client.get(
-                reverse('garpix_user:referral_link', args=[referral_link.referral_hash])
-            )
-            self.assertEqual(response.status_code, 302)
-            self.assertRedirects(response,
-                                 f"{BASE_GARPIX_USER_SETTINGS.get('REFERRAL_REDIRECT_URL', '/')}?status=success")
+class ReferralLinkViewTest(TestCase):
+    def test_referral_links_valid(self):
+        referral_link = ReferralType.objects.create(title='test_link')
+        response = self.client.get(
+            reverse('garpix_user:referral_link', kwargs={'hash': referral_link.referral_hash})
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{BASE_GARPIX_USER_SETTINGS.get('REFERRAL_REDIRECT_URL', '/')}?status=success")
+
+    def test_referral_links_invalid(self):
+        response = self.client.get(
+            reverse('garpix_user:referral_link', kwargs={'hash': 'hash'})
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, f"{BASE_GARPIX_USER_SETTINGS.get('REFERRAL_REDIRECT_URL', '/')}?status=error")
